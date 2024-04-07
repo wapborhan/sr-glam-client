0104e6da3955b57faf3e14fc163ea7d3b05e0114
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-04-07 11:56:40 +0600

    back to top button fix

--- src/home/Homepage.jsx
+++ src/home/Homepage.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import About from "./about/About";
@@ -9,6 +10,29 @@ import FeatureCategories from "./feature/FeatureCategories";
 import Products from "./products/Products";
 
 const Homepage = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Optional smooth scrolling behavior
+    });
+  };
   return (
     <div className="page-wraper" id="scroll-container">
       <Header />
@@ -19,7 +43,14 @@ const Homepage = () => {
       <Adv />
       <Blockbuster />
       <Collection />
-      <Footer />
+      <Footer />{" "}
+      <button
+        className={`scroltop ${isVisible ? "visible" : ""}`}
+        style={{ display: `${isVisible ? "inline-block" : ""}` }}
+        onClick={handleClick}
+      >
+        <i className="fas fa-arrow-up"></i>
+      </button>
     </div>
   );
 };
