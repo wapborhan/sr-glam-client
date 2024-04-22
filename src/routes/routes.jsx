68934375c68bd594b049a4e49b37e3c651b19515
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-04-22 13:29:45 +0600

    Update routes.jsx

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
-// import ComingSoon from "../components/ComingSoon";
 import Homepage from "../home/Homepage";
 import Register from "../layout/Register";
 import Login from "../layout/Login";
@@ -11,7 +10,6 @@ import ProductDetails from "../pages/Shop/details/ProductDetails";
 const routes = createBrowserRouter([
   {
     path: "/",
-    // element: <ComingSoon />,
     element: <Root />,
     errorElement: <Error />,
     children: [
